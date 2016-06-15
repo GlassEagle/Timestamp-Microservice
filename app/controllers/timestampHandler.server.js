e51074631ffda7f18d79ab56664f8f750ce5a9b9
glass eagle <mario.torres2@yahoo.com>
2016-06-15 06:47:24 +0000

    send json for unix timestamp

--- app/controllers/timestampHandler.server.js
+++ app/controllers/timestampHandler.server.js
@@ -4,6 +4,19 @@ module.exports = function (req, res) {
     var timestamp = {
         unix: null,
         natural: null
+    };
+    
+    var param = req.url.substring(1);
+    var num = Number(param);
+    
+    if ((num || num === 0) && Number.isSafeInteger(num)) {
+        var date = new Date(num);
+        var str = month[date.getUTCMonth()] + " " + date.getUTCDate() + ", " + date.getUTCFullYear();
+        timestamp.unix = date.valueOf();
+        timestamp.natural = str;
     }
+    
     res.json(timestamp);
 };
+
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
