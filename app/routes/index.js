dfd3bc3c9b56d1c391b5832783c235716ac979f3
glass eagle <mario.torres2@yahoo.com>
2016-06-15 05:05:29 +0000

    add /* route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,17 @@
 'use strict';
 
 var path = process.cwd();
+var timestampHandler = require(path + "/app/controllers/timestampHandler.server.js");
 
 module.exports = function (app) {
 
 	app.route('/')
 		.get(function (req, res) {
 			res.sendFile(path + '/public/index.html');
+		});
+		
+	app.route('/*')
+		.get(function (req, res) {
+			timestampHandler(req, res);
 		});
 };
