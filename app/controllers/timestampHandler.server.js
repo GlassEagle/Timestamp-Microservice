9becde515df058a2082adf1328f9948ebe4c61a1
glass eagle <mario.torres2@yahoo.com>
2016-06-15 08:10:23 +0000

    send json for natural language date

--- app/controllers/timestampHandler.server.js
+++ app/controllers/timestampHandler.server.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 module.exports = function (req, res) {
     var timestamp = {
@@ -6,17 +6,38 @@ module.exports = function (req, res) {
         natural: null
     };
     
-    var param = req.url.substring(1);
+    var param = decodeURIComponent(req.url.substring(1));
     var num = Number(param);
+    var re = /^([a-z]+) ([0-9]+), ([0-9]+)$/i;
     
     if ((num || num === 0) && Number.isSafeInteger(num)) {
         var date = new Date(num);
         var str = month[date.getUTCMonth()] + " " + date.getUTCDate() + ", " + date.getUTCFullYear();
         timestamp.unix = date.valueOf();
         timestamp.natural = str;
-    }
+    } else if (re.test(param)) {
+        var date = new Date(0);
+        var array = re.exec(param);
+        var monthVal = monthToInt(array[1]);
+        if(monthVal !== -1) {
+            date.setUTCFullYear(array[3], monthVal, array[2]);
+            var str = month[date.getUTCMonth()] + " " + date.getUTCDate() + ", " + date.getUTCFullYear();
+            
+            timestamp.unix = date.valueOf();
+            timestamp.natural = str;
+        }
+    };
     
     res.json(timestamp);
 };
 
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+function monthToInt (str) {
+    for(var i = 0; i < month.length; i++){
+        if(str.toUpperCase() == month[i].toUpperCase()) {
+            return i;
+        }
+    }
+    return -1;
+}
